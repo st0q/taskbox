f20c65e5e30d004e1907ca9baea99015d8af0ae2
st0q <tssk713@gmail.com>
2022-11-07 21:28:14 +0900

    データ要件を明示する

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
@@ -45,3 +46,20 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     </div>
   );
 }
+
+// Note: コンポーネントが想定するデータ構造を示す
+Task.propTypes = {
+   /** Composition of the task */
+   task: PropTypes.shape({
+     /** Id of the task */
+     id: PropTypes.string.isRequired,
+     /** Title of the task */
+     title: PropTypes.string.isRequired,
+     /** Current state of the task */
+     state: PropTypes.string.isRequired,
+   }),
+   /** Event to change the task to archived */
+   onArchiveTask: PropTypes.func,
+   /** Event to change the task to pinned */
+   onPinTask: PropTypes.func,
+  };
