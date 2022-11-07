621345f9e13cdaeddc8ea93a1c9e5da31c60f1be
st0q <tssk713@gmail.com>
2022-11-07 21:19:43 +0900

    設定変更

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,12 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-};
+import '../src/index.css';
+
+// Note: parameters は Storybook の機能やアドオンの振る舞いをコントロールするのに使用します。
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// };
