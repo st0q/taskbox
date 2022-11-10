71bda63b692851e845ab33ed12097c1f5319d8cd
st0q <tssk713@gmail.com>
2022-11-10 21:51:35 +0900

    MSW がリモート API をインターセプトして、適切なレスポンスを返すようにする

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 import '../src/index.css';
+// Registers the msw addon
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 // Note: parameters は Storybook の機能やアドオンの振る舞いをコントロールするのに使用します。
 // export const parameters = {
